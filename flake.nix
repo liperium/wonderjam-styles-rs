cd33731bd371d1a973e94bf37daa69f9287e9e2a
Mattys Gervais <48138387+liperium@users.noreply.github.com>
2024-03-01 15:44:51 -0500

    added dev flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+ description = "Basic dev flake";
+
+ inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+ outputs = { self, nixpkgs }: {
+   devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
+     buildInputs = [
+       cargo
+       gcc
+      
+       # zsh
+     ];
+   };
+ };
+}
